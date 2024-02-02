8f2d9854be840dab10110753f3b84da5baffceed
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2024-02-02 08:37:04 -0500

    V1.5.6

---
+++ config.js
@@ -0,0 +1,4 @@
+var city = `:auto` // :auto for your estimated location (default). City format must be Lansing,MI or London,UK.
+// Types are zipcode, cityname, and geocode.
+var client_id = `DZLMGEFxCvfbQRG7aSN3c`; // This is your client id from aeris weather. 
+var client_secret = `N63dulcmKzQTrWjIrTe2aGKmOw5AhERWWUmjHQKt` // This is your client secret from aeris weather.
