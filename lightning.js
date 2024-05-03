7788dd4048f4421a3f139d36cb4ed983392bdd8f
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2024-05-03 17:42:04 -0400

    V2.3 Build 5 (Our smallest update yet)

--- lightning.js
+++ lightning.js
@@ -14,7 +14,7 @@ async function litening() {
   const responsee = await fetch(alerts)
   const data = await responsee.json()
   const {response} = data
-
+  console.log(data)
   var locs = {
     lat: response[0].loc.lat,
     lon: response[0].loc.long
@@ -22,30 +22,18 @@ async function litening() {
     document.getElementById('caption').innerHTML = `Closest Lightning strike to ${city}`
     document.getElementById("caption").style.textTransform = "capitalize"
     document.getElementById("lightninglocator").innerHTML = `<img style="width: 707px;margin-left: 0px;" src="https://maps.aerisapi.com/${client_id}_${client_secret}/flat-dk,radar,counties,roads,interstates,admin-cities-dk/1500x878/${locs.lat},${locs.lon},10/current.png"/>`
-    document.getElementById("lightning").innerHTML = `Lightning Struck ${response[0].relativeTo.distanceMI} miles (${response[0].relativeTo.distanceKM} km) ${response[0].relativeTo.bearingENG} from ${city}`;
+    document.getElementById("lightning").innerHTML = `Lightning Struck ${response[0].relativeTo.distanceMI} miles (${response[0].relativeTo.distanceKM} km) ${response[0].relativeTo.bearingENG} from ${city}. Struck ${response[0].ob.age} seconds ago. (${response[0].ob.age / 60} minutes ago.)`;
     document.getElementById("zoomedimage").innerHTML = `<img style="width: 707px;margin-top: 30px;" src="https://maps.aerisapi.com/${client_id}_${client_secret}/flat-dk,radar,counties,roads,interstates,admin-cities-dk/1500x878/${locs.lat},${locs.lon},12/current.png"/>`
 }  
 litening()
+setInterval(litening, 1000)
 function homepagelite() {
   window.location = `./index.html?city=${city}`
 }
 async function loadingscreen() {
     document.getElementById("box").style.display = "block",
     document.getElementById("loadingboxm").style.display = "none"
-    
+
 }
 loadingscreen()
 setInterval(loadingscreen, 3008)
-function adjustwidth() {
-  var width = screen.width
-
-  const loadingscreen = document.getElementById("loadingboxm")
-  const mainbox = document.getElementById("box")
-   if (width >= "430px") {
-   loadingscreen.style.height = 3000
-   mainbox.style.width = 1532
-   mainbox.style.height = 1532
-   mainbox.style.font-size === "34px"
-
-   }
-}
