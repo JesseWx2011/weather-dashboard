7581ff40f32ea6e96ea74a981dcf21bd5f885e61
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2024-08-02 08:58:51 -0500

    v2.6 Build 1

--- js/notification.js
+++ js/notification.js
@@ -9,22 +9,22 @@ function notifyMe() {
   litening();
   const alerts = `https://api.aerisapi.com/lightning/flash/${city}?format=json&radius=25mi&minradius=0mi&limit=10&client_id=${client_id}&client_secret=${client_secret} `
   async function litening() {
-     const responsee = await fetch(alerts)
-     const data = await responsee.json()
-     const {response} = data
-     console.log(data)
-     var liteningaudio = "lightning.mp3"
-     if (response[0]?.relativeTo?.distanceMI !== null) {
+    const responsee = await fetch(alerts)
+    const data = await responsee.json()
+    const {response} = data
+    console.log(data)
+    var liteningaudio = "lightning.mp3"
+    if (response[0]?.relativeTo?.distanceMI !== null) {
       document.getElementById("lightningbutton").style.display = "block"
         const notification =  new Notification("Lightning Alert:", {
-             body: `Lightning struck ${response[0].relativeTo.distanceMI} miles (${response[0].relativeTo.distanceKM} km) ${response[0].relativeTo.bearingENG} of this Location in ${city}.`,
-             icon: "lightning.svg",
-             audio: new Audio('lightning.mp3'),
-           });
-         
-         
-           alertaudio.innerHTML = `<audio controls autoplay><source src="${liteningaudio}"></audio>` 
-           if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            body: `Lightning struck ${response[0].relativeTo.distanceMI} miles (${response[0].relativeTo.distanceKM} km) ${response[0].relativeTo.bearingENG} of this Location in ${city}.`,
+            icon: "lightning.svg",
+            audio: new Audio('lightning.mp3'),
+          });
+        
+        
+          alertaudio.innerHTML = `<audio controls autoplay><source src="${liteningaudio}"></audio>` 
+          if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 
           alert(`Lightning struck ${response[0].relativeTo.distanceMI} miles (${response[0].relativeTo.distanceKM} km) ${response[0].relativeTo.bearingENG} of this Location in ${city}.`)
           
@@ -36,20 +36,20 @@ notifyMe()
 alerts();
 const wxalerts = `https://data.api.xweather.com/alerts/${city}?client_id=${client_id}&client_secret=${client_secret}`
 async function alerts() {
- const responsee = await fetch(wxalerts)
- const data = await responsee.json()
- const {response} = data;
- console.log(data)
+const responsee = await fetch(wxalerts)
+const data = await responsee.json()
+const {response} = data;
+console.log(data)
 
   alert = response[0].details.name
   starttimeold = response[0].timestamps.beginsISO
   endtimeold = response[0].timestamps.expiresISO
   tz = response[0].profile.tz
- starttime = new Date(starttimeold).toLocaleString("en-US", {timeStyle: "short", timeZone: tz, });
- endtime = new Date(endtimeold).toLocaleString("en-US", {timeStyle: "short", timeZone: tz, });
+starttime = new Date(starttimeold).toLocaleString("en-US", {timeStyle: "short", timeZone: tz, });
+endtime = new Date(endtimeold).toLocaleString("en-US", {timeStyle: "short", timeZone: tz, });
   console.log(starttime)
   if (alert !== null) {
-     alertnotificaton = new Notification("Weather Alert:", {
+    alertnotificaton = new Notification("Weather Alert:", {
       body: `${alert} in effect from ${starttime} and ending at ${endtime}`,
       icon: "alert.svg",
     })};
